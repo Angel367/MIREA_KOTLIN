278605b060ffc63847bc5ceabced0134d2422ae4
Egor Volkov <egor.volkov20038@gmail.com>
2023-10-12 22:08:15 +0300

    finished

---
+++ Main.kt
@@ -0,0 +1,17 @@
+interface Movable {
+    fun move( dx: Int, dy: Int)
+}
+
+internal class Point(var x:Int, var y:Int) : Movable {
+    override fun move(dx: Int, dy: Int) {
+        x += dx
+        y += dy
+    }
+}
+internal class CloudOfPoints(val points: ArrayList<Point>) : Movable {
+    override fun move(dx: Int, dy: Int) {
+        for (point in points) {
+            point.move(dx, dy)
+        }
+    }
+}
